14692ec7b0a00d95721d362469ff61b407671431
AndriusK <andrius.k@sanitex.eu>
2016-05-16 09:36:53 +0300

    .net 4.6

--- build.cmd
+++ build.cmd
@@ -1,1 +1,8 @@
-call msbuild
+@set msbuild="%ProgramFiles%\msbuild\14.0\Bin\MSBuild.exe"
+@if not exist %msbuild% @set msbuild="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
+@if not exist %msbuild% @set msbuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
+@if not exist %msbuild% @set msbuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+
+%msbuild% libcheck.sln /v:m /m
+
+@PAUSE
