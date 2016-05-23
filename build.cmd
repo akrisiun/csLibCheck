ea54f3f5c5700f59d0b5db4c5aeccccee2035844
AndriusK <andrius.k@sanitex.eu>
2016-05-23 12:27:02 +0300

    libcheck.project.json for new Nuget

--- build.cmd
+++ build.cmd
@@ -3,6 +3,11 @@
 @if not exist %msbuild% @set msbuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
 @if not exist %msbuild% @set msbuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
 
+nuget restore libcheck.sln
+cd src\
+dotnet restore
+cd ..\
+
 %msbuild% libcheck.sln /v:m /m
 
 @PAUSE
