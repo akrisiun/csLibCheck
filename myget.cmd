ff096811121131a04447ed128aa37773f76505a9
AndriusK <akrisiun@gmail.com>
2017-04-30 20:04:39 +0300

    LibCheck myget build, v1.0.4

--- myget.cmd
+++ myget.cmd
@@ -1,27 +1,31 @@
 set dir=%~dp0
 @set cfg=Debug
-set build=1.0.3
+set build=1.0.4
 
 @echo -------------------------------------------------------------------
 cd "%dir%"
-type myget.cmd
+@REM type myget.cmd
 @echo -------------------------------------------------------------------
 
 @REM myget.org build server
+@set MsBuildExe=%ProgramFiles(x86)%\MSBuild\15.0\Bin\MSBuild.exe
 
-@if "%MsBuildExe%"=="" (
-@set MsBuildExe=%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe
-)
-@if not exist "%MsBuildExe%" @set MsBuildExe=%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe
-@if not exist "%MsBuildExe%" @set MsBuildExe=%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe
+@if not exist "%MsBuildExe%" @set MsBuildExe=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
+@if not exist "%MsBuildExe%" @set MsBuildExe=%ProgramFiles(x86)%\MSBuild\15.0\Bin\MSBuild.exe
 @if not exist "%MsBuildExe%" @set MsBuildExe=%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe
-
 
 @echo "%MsBuildExe%"
 
-.nuget\nuget.exe restore libcheck.sln
+mkdir Packages
 
-"%MsBuildExe%" /p:Configuration="%cfg%" /v:m /m  libcheck.sln
+set NURL=https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+set NUGET=Packages\nuget.exe
+if not exist %~dp0%NUGET% @powershell Invoke-WebRequest %NURL% -OutFile %NUGET%
 
-.nuget\nuget.exe pack src\CsLibCheck.nuspec -Version %build%
+%NUGET% restore libcheck.sln
+
+@REM "%MsBuildExe%" /p:Configuration="%cfg%" /v:m /m  libcheck.sln
+
+%NUGET% pack src\LibCheck.csproj -Version %build% -Symbols -Build
+@REM CsLibCheck.nuspec --no-build --include-symbols
 
